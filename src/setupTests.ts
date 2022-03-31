c1bd8835c68a20998e20be10017ebfa5af1abcce
Heritier Mbumba <heritiermbumba@Heritier-Mbumba-2.local>
2022-03-31 18:36:20 +0200

    add robot test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning: ReactDOM.render is no longer supported in React 18./.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
